e9eb09f976513f8a685bcd7205a3d2a646a80f33
Roman <kvintaslev@i.ua>
2019-06-26 17:50:31 +0300

    second commit

--- app.js
+++ app.js
@@ -4,22 +4,21 @@ const jsonParser = express.json();
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-    
-    author_id: String,
-    title: String,
-    avatar_path: String,
-    pages_num: Number,
-    short_desc: String,
-    publisher: String,
-    publish_year: Number
+const bookSchema = new Schema({    
+    author_id: {type: String,required: true},
+    title: {type: String, required: true},
+    avatar_path: {type: String, required: true},
+    pages_num: {type: Number, required: true},
+    short_desc: {type: String, required: true},
+    publisher: {type: String, required: true},
+    publish_year: {type: Number, required: true}
 });
 
 const authorSchema = new Schema({
-   full_name: String,
-   nationality: String,
-   avatar_path_author: String,
-   short_bio: String
+   full_name: {type: String, required: true},
+   nationality: {type: String, required: true},
+   avatar_path_author: {type: String, required: true},
+   short_bio: {type: String, required: true}
 });
 
 const Book = mongoose.model("Book", bookSchema);
@@ -38,18 +37,19 @@ app.get("/books", (req, res)=>{
     var size = parseInt(req.query.size)
     var query = {}
     if (pageNo < 0 || pageNo === 0) {
-        response = { "error": true, "message": "invalid page number, should start with 1" };
-        return res.json(response)
+        
+       return res.status(400).send(`PageNo must be more than 0`)
     }
     query.skip = size * (pageNo - 1)
     query.limit = size
     Book.find({}, {}, query, function (err, data) {
         if (err) {
-            response = { "error": true, "message": "Error fetching data" };
+            res.status(400).send(`Error fetching data`);
         } else {
-            response = { "List of books": data };
-        }
-        res.json(response);
+            res.status(200).send(`List of books  
+            ${data}`);
+        };
+        
     });
 });
 
@@ -78,23 +78,6 @@ app.post("/books", jsonParser, (req, res)=>{
     });
 });
 
-app.put("/books", jsonParser,(req,res)=>{
-    if(!req.body) return res.sendStatus(400);
-    Book.findOneAndUpdate({_id: req.body._id}, {
-        author_id: req.body.author_id,
-        title: req.body.title,
-        avatar_path: req.body.avatar_path,
-        pages_num: req.body.pages_num,
-        short_desc: req.body.short_desc,
-        publisher: req.body.publisher,
-        publish_year: req.body.publish_year 
-    }, 
-    {new:true},
-    (err, doc)=>{
-        if(err) return console.log(err);
-        res.send(doc);
-    });
-});
 
 app.delete("/books/:id", (req, res)=>{
     Book.findOneAndDelete({_id: req.params.id}, (err, doc)=>{
@@ -147,20 +130,7 @@ app.post("/authors", jsonParser, (req, res)=>{
     });
 });
 
-app.put("/author", jsonParser,(req,res)=>{
-    if(!req.body) return res.sendStatus(400);
-    Author.findOneAndUpdate({_id: req.body.id}, {
-        full_name: req.body.full_name,
-        nationality: req.body.nationality,
-        avatar_path_author: req.body.avatar_path_author,
-        short_bio: req.body.short_bio 
-    }, 
-    {new:true},
-    (err, doc)=>{
-        if(err) return console.log(err);
-        res.send(doc);
-    });
-});
+
 
 app.delete("/authors/:id", (req, res)=>{
     Author.findOneAndDelete({_id: req.params.id}, (err, doc)=>{
